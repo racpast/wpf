cfc311fe0a9c8d57b4390852f19892c0b3aa41d4
racpast <racpast@gmail.com>
2026-02-24 23:13:00 +0800

    refactor(interop): migrate SafeNativeMethodsMilCoreApi to LibraryImport

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/SafeNativeMethodsMilCoreApi.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/SafeNativeMethodsMilCoreApi.cs
@@ -4,44 +4,44 @@
 using System.Runtime.InteropServices;
 
 //
-//  ABOUT THIS FILE:
-//   -- This file contains native methods which are deemed SAFE for partial trust callers
-//   -- These methods DO have the SuppressUnmanagedCodeSecurity attribute which means 
-//       stalk walks for unmanaged 
-//      code will stop with the immediate caler. 
-//   -- Put methods in here which are needed in partial trust scenarios
-//   -- If you have questions about how to use this file, email avsee
+//  ABOUT THIS FILE:
+//   -- This file contains native methods which are deemed SAFE for partial trust callers
+//   -- These methods DO have the SuppressUnmanagedCodeSecurity attribute which means 
+//       stalk walks for unmanaged 
+//      code will stop with the immediate caler. 
+//   -- Put methods in here which are needed in partial trust scenarios
+//   -- If you have questions about how to use this file, email avsee
 
 namespace MS.Win32.PresentationCore
 {
-    internal static partial class SafeNativeMethods
-    {
-       internal static int MilCompositionEngine_InitializePartitionManager(int nPriority)
-       {
-            return SafeNativeMethodsPrivate.MilCompositionEngine_InitializePartitionManager(nPriority);
-       }
+    internal static partial class SafeNativeMethods
+    {
+       internal static int MilCompositionEngine_InitializePartitionManager(int nPriority)
+       {
+            return SafeNativeMethodsPrivate.MilCompositionEngine_InitializePartitionManager(nPriority);
+       }
 
-       internal static int MilCompositionEngine_DeinitializePartitionManager()
-       {
-            return SafeNativeMethodsPrivate.MilCompositionEngine_DeinitializePartitionManager();
-       }
+       internal static int MilCompositionEngine_DeinitializePartitionManager()
+       {
+            return SafeNativeMethodsPrivate.MilCompositionEngine_DeinitializePartitionManager();
+       }
 
-       internal static long GetNextPerfElementId()
-       {
-           return SafeNativeMethodsPrivate.GetNextPerfElementId();
-       }
+       internal static long GetNextPerfElementId()
+       {
+           return SafeNativeMethodsPrivate.GetNextPerfElementId();
+       }
 
-       private static partial class SafeNativeMethodsPrivate
-       {
-            [DllImport(DllImport.MilCore)]
-            internal static extern int MilCompositionEngine_InitializePartitionManager(int nPriority);
+       private static partial class SafeNativeMethodsPrivate
+       {
+            [LibraryImport(DllImport.MilCore)]
+            internal static partial int MilCompositionEngine_InitializePartitionManager(int nPriority);
 
-            [DllImport(DllImport.MilCore)]
-            internal static extern int MilCompositionEngine_DeinitializePartitionManager();
+            [LibraryImport(DllImport.MilCore)]
+            internal static partial int MilCompositionEngine_DeinitializePartitionManager();
 
-            [DllImport(DllImport.MilCore)]
-            internal static extern long GetNextPerfElementId();
-       }
-    }
+            [LibraryImport(DllImport.MilCore)]
+            internal static partial long GetNextPerfElementId();
+       }
+    }
 }
 
